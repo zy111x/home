7f90b147634b6c697dd8675a45ce306daeaadab0
zy111x <150530845+zy111x@users.noreply.github.com>
2024-09-29 10:29:47 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, MailBulk, Images } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, MailBulk, Images, SortAmountDown, HourglassStart } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -72,6 +72,8 @@ const siteIcon = {
   LaptopCode,
   MailBulk,
   Images,
+  SortAmountDown,
+  HourglassStart,
 };
 
 // 链接跳转
